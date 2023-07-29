5df953e8ce2cc6a76a4d243b68f5ecc7217adcf8
Seth Phat <phattranminh96@gmail.com>
2023-07-29 16:57:09 +0700

    Laravel Inbox Pattern - ShipSaaS

--- src/Http/Controllers/InboxController.php
+++ src/Http/Controllers/InboxController.php
@@ -2,11 +2,15 @@
 
 namespace ShipSaasInboxProcess\Http\Controllers;
 
+use Illuminate\Database\QueryException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Log;
 use ShipSaasInboxProcess\Http\Requests\AbstractInboxRequest;
 use ShipSaasInboxProcess\InboxProcessSetup;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 class InboxController extends Controller
 {
@@ -20,11 +24,39 @@ class InboxController extends Controller
         $inboxRequest = InboxProcessSetup::getRequest($topic)::createFrom($request);
 
         // insert inbox msg
-        appendInboxMessage(
-            $topic,
-            $inboxRequest->getInboxExternalId(),
-            $inboxRequest->getInboxPayload()
-        );
+        try {
+            appendInboxMessage(
+                $topic,
+                $inboxRequest->getInboxExternalId(),
+                $inboxRequest->getInboxPayload()
+            );
+        } catch (QueryException $exception) {
+            // 23000: mysql unique
+            // 23505: pgsql unique_violation
+            if (in_array($exception->getCode(), ['23000', '23505'])) {
+                return new JsonResponse(['error' => 'duplicated'], 409);
+            }
+
+            // gratefully log for recovery purpose
+            report($exception);
+            Log::warning('Failed to append inbox message', [
+                'topic' => $topic,
+                'external_id' => $inboxRequest->getInboxExternalId(),
+                'payload' => $inboxRequest->getInboxPayload(),
+            ]);
+
+            return new JsonResponse(['error' => 'unknown'], 400);
+        } catch (Throwable $throwable) {
+            // gratefully log for recovery purpose
+            report($throwable);
+            Log::warning('Failed to append inbox message', [
+                'topic' => $topic,
+                'external_id' => $inboxRequest->getInboxExternalId(),
+                'payload' => $inboxRequest->getInboxPayload(),
+            ]);
+
+            return new JsonResponse(['error' => 'unknown'], 400);
+        }
 
         $response = InboxProcessSetup::getResponse($topic);
 
