b957b7880b3e9cc77fc94199314f7677905d1aa7
Seth Phat <phattranminh96@gmail.com>
2023-08-19 20:29:12 +0700

    Laravel Inbox Pattern - ShipSaaS

--- tests/Unit/Repositories/RunningInboxRepositoryTest.php
+++ tests/Unit/Repositories/RunningInboxRepositoryTest.php
@@ -2,9 +2,43 @@
 
 namespace ShipSaasInboxProcess\Tests\Unit\Repositories;
 
+use ShipSaasInboxProcess\Repositories\RunningInboxRepository;
 use ShipSaasInboxProcess\Tests\TestCase;
 
 class RunningInboxRepositoryTest extends TestCase
 {
+    public function testAcquireLockSuccessfully()
+    {
+        $repo = new RunningInboxRepository();
 
+        $acquiredLock = $repo->acquireLock('test');
+
+        $this->assertTrue($acquiredLock);
+
+        $this->assertDatabaseHas('running_inboxes', [
+            'topic' => 'test',
+        ]);
+    }
+
+    public function testAcquireLockFailed()
+    {
+        $repo = new RunningInboxRepository();
+
+        $repo->acquireLock('test');
+        $acquiredLock = $repo->acquireLock('test');
+
+        $this->assertFalse($acquiredLock);
+    }
+
+    public function testUnlockSuccessfully()
+    {
+        $repo = new RunningInboxRepository();
+
+        $repo->acquireLock('test');
+        $repo->unlock('test');
+
+        $this->assertDatabaseMissing('running_inboxes', [
+            'topic' => 'test',
+        ]);
+    }
 }
